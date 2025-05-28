b909defffed2465de309c35e254e1cc2a4a0f631
GHonda-I-75 <ITVanTampa@gmail.com>
2025-05-28 15:35:41 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,101 @@
+provider "aws" {
+  region = "us-east-1"
+}
 
+# === IAM Role for Lambda ===
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "mythical_lambda_exec_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
+      Effect = "Allow",
+      Sid    = ""
+    }]
+  })
+}
+
+resource "aws_iam_policy_attachment" "lambda_logs" {
+  name       = "attach-lambda-logs"
+  roles      = [aws_iam_role.lambda_exec_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# === S3 Bucket ===
+resource "aws_s3_bucket" "data_bucket" {
+  bucket = "mythical-realms-data-${random_id.suffix.hex}"
+  force_destroy = true
+}
+
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+# === DynamoDB Table ===
+resource "aws_dynamodb_table" "guest_flow" {
+  name         = "MythicalGuestFlow"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "guest_id"
+
+  attribute {
+    name = "guest_id"
+    type = "S"
+  }
+}
+
+# === Lambda Function ===
+resource "aws_lambda_function" "predict_wait_time" {
+  function_name = "predictWaitTime"
+  role          = aws_iam_role.lambda_exec_role.arn
+  handler       = "handler.lambda_handler"
+  runtime       = "python3.11"
+  filename      = "${path.module}/lambda_function.zip"  # Zip your Lambda code first
+
+  environment {
+    variables = {
+      DYNAMODB_TABLE = aws_dynamodb_table.guest_flow.name
+      BUCKET_NAME    = aws_s3_bucket.data_bucket.bucket
+    }
+  }
+}
+
+# === API Gateway ===
+resource "aws_apigatewayv2_api" "http_api" {
+  name          = "predict-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_lambda_permission" "apigw_invoke" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.predict_wait_time.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id           = aws_apigatewayv2_api.http_api.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = aws_lambda_function.predict_wait_time.invoke_arn
+}
+
+resource "aws_apigatewayv2_route" "lambda_route" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "GET /predict"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+# === Outputs ===
+output "api_endpoint" {
+  value = aws_apigatewayv2_api.http_api.api_endpoint
+}
