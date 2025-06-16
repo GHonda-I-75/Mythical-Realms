5428cf4015710bc14cb6ffa6bb73612aacb46ee2
Evan <ITVanTampa@gmail.com>
2025-06-16 15:39:52 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -112,5 +112,5 @@ resource "aws_apigatewayv2_stage" "default" {
 
 # === Outputs ===
 output "api_endpoint" {
-  value = aws_apigatewayv2_api.http_api.api_endpoint
+  value = "${aws_apigatewayv2_api.http_api.api_endpoint}/predict"
 }
